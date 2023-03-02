d67774253abf7e740475663bdb081d75503dc355
minuxx <minux8905@gmail.com>
2023-03-02 21:02:49 +0900

    feat: 질문지 준비 완료 화면 라우터 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Onboarding from './pages/Onboarding'
 import Research from './pages/Research'
+import ResearchReady from './pages/ResearchReady'
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Route path="/" element={<Landing />} />
         <Route path="/onboarding" element={<Onboarding />} />
         <Route path="/research" element={<Research />} />
+        <Route path="/research/ready" element={<ResearchReady />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/ResearchReady.jsx
+++ src/pages/ResearchReady.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../components/header/Header'
-import { Title } from '../components/texts/Texts'
+import { TextDesc, Title } from '../components/texts/Texts'
 
 function ResearchReady() {
   return (
     <div>
       <Header></Header>
       <Title>질문지 준비 완료</Title>
+      <TextDesc>
+        상대에게 질문지를 보낼 준비가 완료되었어요 링크를 공유하고 답변을
+        요청해보세요
+      </TextDesc>
     </div>
   )
 }
