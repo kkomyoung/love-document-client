558e8ad54a5beb3b254ff0ab8a10964c1423f1b1
kkomyoung <kya754@gmail.com>
2023-03-09 12:32:30 +0900

    chore: lottie 테스트

--- src/pages/StandardComplete.jsx
+++ src/pages/StandardComplete.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import Header from '../components/header/Header'
 import { ButtonArea, RoundButton } from '../components/buttons/Buttons'
 import { Title, TextDesc } from '../components/texts/Texts'
-import ImgTest from '../assets/img_test.png'
-function StandardComplete() {
+import lottie from 'lottie-web'
+import animationData from '../assets/lottie/heart_lottie.json'
+
+const LottieAnimation = () => {
+  const container = useRef(null)
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      animationData,
+    })
+  }, [])
+
+  return <div ref={container}></div>
+}
+
+const StandardComplete = () => {
   return (
     <StyledMain>
       <Header btnClose="/" />
       <StyledAirticle>
         <StyledImgWrap>
-          <img src={ImgTest} alt="" />
+          <LottieAnimation />
         </StyledImgWrap>
         <Title>내 기준 작성완료</Title>
         <TextDesc>
@@ -29,6 +44,7 @@ function StandardComplete() {
 
 const StyledMain = styled.main`
   padding-bottom: 4.8rem;
+  background: pink;
 `
 
 const StyledAirticle = styled.article`
@@ -39,11 +55,6 @@ const StyledImgWrap = styled.figure`
   width: 100%;
   margin-bottom: 4.3rem;
   text-align: center;
-
-  img {
-    width: 100%;
-    max-width: 31.2rem;
-  }
 `
 
 export default StandardComplete
