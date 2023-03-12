650d063472f17f7d0096a26b6b6cdcd5c42707bc
minuxx <minux8905@gmail.com>
2023-03-12 17:10:25 +0900

    feat: 헤더/타이틀 추가

--- src/pages/Standard.jsx
+++ src/pages/Standard.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 import Header from '../components/header/Header'
+import { TextArea, TextDesc, Title } from '../components/texts/Texts'
 
 function Standard() {
   return (
     <StyledMain>
       <Header btnBack />
-      <StyledAirticle></StyledAirticle>
+      <StyledAirticle>
+        <TextArea>
+          <Title>내 기준 작성하기</Title>
+          <TextDesc>
+            <span>내 애인후보.. 이것만큼은.. 절대 지켜!!</span>
+            <br />
+            <span>상대방에게 바라는 조건을 작성해주세요</span>
+          </TextDesc>
+        </TextArea>
+      </StyledAirticle>
     </StyledMain>
   )
 }
