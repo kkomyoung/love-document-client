dcd56fb53b4e714aebf1fa2b11e67098478e03d6
kkomyoung <kya754@gmail.com>
2023-03-23 22:31:25 +0900

    feat: 질문지 카테고리 api 연동

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -15,35 +15,18 @@ import {
   TextDesc,
 } from '../components/texts/Texts'
 
-const dummy = [
-  {
-    id: 1,
-    title: '외모',
-    items: ['키', '체형', '쌍커풀', '피부', '안경', '타투'],
-  },
-  {
-    id: 2,
-    title: '가치관',
-    items: ['종교', '결혼관', '자녀관', '정치성향', '술', '흡연'],
-  },
-  {
-    id: 3,
-    title: '연애스타일',
-    items: ['연애리스크', '만남 빈도', '연락 빈도', '스킨십', '표현'],
-  },
-]
 function Research() {
   const [openModal, setOpenModal] = useState(false)
 
-  const { isLoading, error, data } = useQuery(['todos'], () =>
-    fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+  const { isLoading, error, data } = useQuery(['categories'], () =>
+    fetch('http://api-dev.love-document.com/categories').then((res) =>
       res.json()
     )
   )
 
   if (isLoading) return 'Loading...'
 
-  if (error) return `An error has occurred: ${error.message}`
+  if (error) return console.log(error.message)
 
   const showModal = () => {
     setOpenModal(true)
@@ -52,11 +35,6 @@ function Research() {
     <StyledMain>
       <Header title="질문지 만들기" btnBack />
       <article>
-        <div>
-          {data.map((todo) => (
-            <div key={todo.id}>{todo.title}</div>
-          ))}
-        </div>
         <TextArea>
           <Title>질문지 만들기</Title>
           <TextDesc>
@@ -66,7 +44,7 @@ function Research() {
           </TextDesc>
         </TextArea>
         <StyledSectionCategory>
-          <CategoryBoxList data={dummy}></CategoryBoxList>
+          <CategoryBoxList data={data}></CategoryBoxList>
         </StyledSectionCategory>
         <StyledSectionLogin>
           <SubTextArea>
