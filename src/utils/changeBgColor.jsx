19de4881787d6f01ca676db24061cbc3f1b2ad64
kkomyoung <kya754@gmail.com>
2023-03-31 02:10:31 +0900

    feat: meta theme-color 수정

--- src/utils/changeBgColor.jsx
+++ src/utils/changeBgColor.jsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react'
 
 function changeBgColor(color) {
   useEffect(() => {
+    document.querySelector("meta[name='theme-color']").content = color
     document.querySelector('#wrap').style.backgroundColor = color
 
     return () => {
+      document.querySelector("meta[name='theme-color']").content = '#fff'
       document.querySelector('#wrap').style.backgroundColor = ''
     }
   }, [])
