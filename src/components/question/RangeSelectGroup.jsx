e959c074f16a38b54d540185a97fd000d96a6fcd
minuxx <minux8905@gmail.com>
2023-06-04 22:36:27 +0900

    style: RangeSelectGroup 컴포넌트 UI 구성

--- src/components/question/RangeSelectGroup.jsx
+++ src/components/question/RangeSelectGroup.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+
 import { SELECT_OPTIONS } from '../../utils/constants'
 
-function QuestionSelect({ questionId, dataType }) {
+function RangeSelectGroup({
+  questionId,
+  questionType,
+  answeredRange,
+  dataType,
+}) {
   const [selected, setSelected] = useState('')
 
   const handleSelect = (e) => {
@@ -21,26 +27,53 @@ function QuestionSelect({ questionId, dataType }) {
           </Option>
         ))}
       </Select>
+
+      <span>이상</span>
+
+      <Select onChange={handleSelect} value={selected}>
+        <Option value="" disabled selected hidden>
+          {dataType}
+        </Option>
+        {SELECT_OPTIONS[dataType].map((item, index) => (
+          <Option value={item} key={index}>
+            {item}
+          </Option>
+        ))}
+      </Select>
+
+      <span>이하</span>
     </Box>
   )
 }
 
-export default QuestionSelect
+export default RangeSelectGroup
 
 const Box = styled.div`
   display: flex;
-  border: 1px solid ${(props) => props.theme.gray500};
-  border-radius: 0.8rem;
-  overflow: hidden;
-  height: 4.8rem;
+  align-items: center;
+  justify-content: center;
+
+  select {
+    margin-right: 0.8rem;
+  }
+
+  span {
+    margin-right: 1.2rem;
+    ${(props) => props.theme.fontSize.label_m_m}
+    color: ${(props) => props.theme.gray800}
+  }
 `
 
 const Select = styled.select`
+  width: 8rem;
+  height: 4.8rem;
   font-size: 1.6rem;
   flex-grow: 1;
-  height: 100%;
-  border: none;
+  border-radius: 0.8rem;
+  border: 1px solid ${(props) => props.theme.gray500};
+  overflow: hidden;
   padding: 0 1.2rem;
+  outline: none;
   ${(props) => props.theme.fontSize.lable_m_m}
   color: ${(props) =>
     props.value === '' ? props.theme.gray400 : props.theme.gray900};
